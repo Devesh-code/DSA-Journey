ef0c32d72ae75415449856960c7cc2a20869a010
Devesh-Code <developersharma1999@gmail.com>
2023-10-24 23:16:37 +0530

    DSA

---
+++ Day-1(P3)leetcodeEasy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        std::vector<std::vector<int>> res;
+        if (numRows <= 0) {
+            return res;
+        }
+
+        res.push_back({1});
+        for (int i = 1; i < numRows; i++) {
+            const std::vector<int>& prev = res.back();
+            std::vector<int> row;
+            row.push_back(1);
+
+            for (int j = 1; j < prev.size(); j++) {
+                row.push_back(prev[j - 1] + prev[j]);
+            }
+
+            row.push_back(1);
+            res.push_back(row);
+        }
+
+        return res;
+
+
+
+
+
+    }
+};
