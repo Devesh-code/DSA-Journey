2d80d4d5c9d8c9d7cea03a085aecbf1a2bf0bdc4
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-02 23:43:51 +0530

    Time: 196 ms (40.74%) | Memory: 20.7 MB (30.30%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,18 +1,18 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        l, r = 0, len(s) - 1
-        while l < r:                # this approach have T: O(n) & S: O(1)
-            s[l], s[r] = s[r], s[l]
-            l, r = l + 1, r - 1
+        # l, r = 0, len(s) - 1
+        # while l < r:                # this approach have T: O(n) & S: O(1)
+        #     s[l], s[r] = s[r], s[l]
+        #     l, r = l + 1, r - 1
 
 # Using a stack for reversing the string But it have T: O(n) and S: O(n)
-        # stack = []
-        # for c in s:
-        #     stack.append(c):
-        # i = 0
-        # while stack:
-        #     s[i] = stack.pop()
-        #     i += 1
+        stack = []
+        for c in s:
+            stack.append(c)
+        i = 0
+        while stack:
+            s[i] = stack.pop()
+            i += 1
         
 # # Using Recursive Function for do the swap without using another while loop, insted of the loop we recalling over reverse function with new left and right arguments
 
