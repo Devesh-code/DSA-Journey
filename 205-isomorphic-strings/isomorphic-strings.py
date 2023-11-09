f8d04d327937c9146ae701a36f94682d64c1c532
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-09 23:31:54 +0530

    Time: 42 ms (79.97%) | Memory: 16.4 MB (82.68%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,19 +1,29 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapST, mapTS = {}, {}  # Initialize two empty dictionaries to map characters from 's' to 't' and vice versa.
+        # Create a set 'zipped' by combining characters from 's' and 't' pairwise.
+        zipped = set(zip(s, t))
+        
+        # Check if the length of 'zipped' is equal to the length of unique characters in 's' and 't'.
+        if len(zipped) == len(set(s)) == len(set(t)):
+            return True  # Return True if the strings are isomorphic.
+        else:
+            return False  # Return False if they are not isomorphic.
 
-# Iterate through corresponding characters in 's' and 't' using the zip function.
-        for c1, c2 in zip(s, t):
-    # Check if 'c1' is already mapped to a different character in 't' or if 'c2' is already mapped to a different character in 's'.
-            if (c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1):
-                return False  # Return False if there is an inconsistency in the mappings.
+#         mapST, mapTS = {}, {}  # Initialize two empty dictionaries to map characters from 's' to 't' and vice versa.
 
-    # Map 'c1' from 's' to 'c2' in 't'.
-            mapST[c1] = c2
+# # Iterate through corresponding characters in 's' and 't' using the zip function.
+#         for c1, c2 in zip(s, t):
+#     # Check if 'c1' is already mapped to a different character in 't' or if 'c2' is already mapped to a different character in 's'.
+#             if (c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1):
+#                 return False  # Return False if there is an inconsistency in the mappings.
 
-    # Map 'c2' from 't' to 'c1' in 's'.
-            mapTS[c2] = c1
+#     # Map 'c1' from 's' to 'c2' in 't'.
+#             mapST[c1] = c2
 
-        return True  # If all characters are consistently mapped in both directions, return True.
+#     # Map 'c2' from 't' to 'c1' in 's'.
+#             mapTS[c2] = c1
+
+#         return True  # If all characters are consistently mapped in both directions, return True.
+
 
         
