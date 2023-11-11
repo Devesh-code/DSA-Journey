eb0200c474c288b07ec741afb5158f79845efa6b
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-12 01:28:37 +0530

    Time: 70 ms (37.38%) | Memory: 16.7 MB (10.13%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,25 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ## Define the lowercase letters of the alphabet.
-        letters = 'abcdefghijklmnopqrstuvwxyz'
+        char_count = {}  # Dictionary to store character counts.
 
-        # Initialize a dictionary 'counter' with letters as keys and 0 as initial count.
-        counter = {letter: 0 for letter in letters}
-
-        # Traverse the 'magazine' string and count occurrences of each letter.
+        # Populate the char_count dictionary with counts from the magazine.
         for char in magazine:
-            counter[char] += 1
-        
-        # Traverse the 'ransomNote' string and decrement the count of each letter.
+            char_count[char] = char_count.get(char, 0) + 1
+
+        # Check if ransomNote can be constructed from magazine.
         for char in ransomNote:
-            counter[char] -= 1
+            # Check if the character is in char_count and has a count greater than or equal to 1.
+            if char in char_count and char_count[char] >= 1:
+                char_count[char] -= 1  # Decrement the count of the character in char_count.
+            else:
+                return False  # If the character is not available or count is 0, return False.
 
-        # Check if any count in 'counter' is negative, indicating insufficient characters.
-        for char, count in counter.items():
-            if counter[char] < 0:
-                return False
-
-        # If all characters in 'ransomNote' can be constructed, return True.
-        return True
+        return True  # If all characters in ransomNote can be constructed, return True.
 
         
