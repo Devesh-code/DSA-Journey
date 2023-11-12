6fe3e6cf349b62c9389f92fbe752dbdbff77f7b5
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-13 01:22:07 +0530

    Time: 20 ms (86.72%) | Memory: 10.9 MB (81.56%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int c[26] {}, i = 0;
-        for (; i < s.size(); c[s[i++] - 'a']++);
-        for (i = 0; i < s.size() && c[s[i] - 'a'] != 1; i++);
-        return i < s.size() ? i : -1;
+       int c[26] {};  // Initialize an array 'c' to store the frequency of each character (initialized to zero).
+int i = 0;  // Initialize a variable 'i' to zero.
+
+// First loop: Count the frequency of each character in the string 's'.
+for (; i < s.size(); c[s[i++] - 'a']++);
+
+// Second loop: Find the index of the first non-repeating character.
+for (i = 0; i < s.size() && c[s[i] - 'a'] != 1; i++);
+
+// Return the result: If a non-repeating character is found, return its index; otherwise, return -1.
+return i < s.size() ? i : -1;
+
+
+
     //     int n = s.length();  // Get the length of the string.
     //     vector<int> frequency(26, 0);  // Initialize a vector to store the frequency of each character.
 
