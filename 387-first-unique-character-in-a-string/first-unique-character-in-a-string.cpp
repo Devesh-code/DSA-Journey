96774be09db2fca040dd8bffa5360b71f2843975
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-13 01:19:26 +0530

    Time: 16 ms (95.41%) | Memory: 10.9 MB (94.83%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int n = s.length();  // Get the length of the string.
-        vector<int> frequency(26, 0);  // Initialize a vector to store the frequency of each character.
+        int c[26] {}, i = 0;
+        for (; i < s.size(); c[s[i++] - 'a']++);
+        for (i = 0; i < s.size() && c[s[i] - 'a'] != 1; i++);
+        return i < s.size() ? i : -1;
+    //     int n = s.length();  // Get the length of the string.
+    //     vector<int> frequency(26, 0);  // Initialize a vector to store the frequency of each character.
 
-        // Traverse the string and update the frequency vector.
-        for (int i = 0; i < n; ++i) {
-            frequency[s[i] - 'a']++;
-        }
+    //     // Traverse the string and update the frequency vector.
+    //     for (int i = 0; i < n; ++i) {
+    //         frequency[s[i] - 'a']++;
+    //     }
 
-        // Traverse the string again to find the index of the first non-repeating character.
-        for (int i = 0; i < n; ++i) {
-            if (frequency[s[i] - 'a'] == 1) {
-                return i;  // Return the index of the first non-repeating character.
-            }
-        }
+    //     // Traverse the string again to find the index of the first non-repeating character.
+    //     for (int i = 0; i < n; ++i) {
+    //         if (frequency[s[i] - 'a'] == 1) {
+    //             return i;  // Return the index of the first non-repeating character.
+    //         }
+    //     }
 
-        // If no non-repeating character is found, return -1.
-        return -1;
+    //     // If no non-repeating character is found, return -1.
+    //     return -1;
     }
 };
