0eaabd417256530e4a81902d48bde018b5c6cbbb
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-15 13:41:31 +0530

    Time: 35 ms (94.48%) | Memory: 16.3 MB (29.34%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,5 +1,5 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         complete_rows = int(((2 * n) + 0.25)**0.5 - 0.5)
-        return complete_rows
+        return complete_rows 
         
