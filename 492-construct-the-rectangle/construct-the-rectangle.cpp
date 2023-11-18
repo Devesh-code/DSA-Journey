4922b11bcb7c7a67125bdfed33793b25e84597eb
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-19 01:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (57.13%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.cpp
+++ 492-construct-the-rectangle/construct-the-rectangle.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        
-    int x = sqrt(area);
-
-    while (true) {
-   
-    int y = area / x;
-
-    
-    if (area % x == 0) {
-        if (x > y){
-            return {x,y};
-        }
-     else {
-        return {y, x};
-    }
-    }
-    x++;
-}
-
+       int x = sqrt(area);
+      while (area % x != 0) {
+         x--;
+      }
+      return {area / x, x};
     }
 };
