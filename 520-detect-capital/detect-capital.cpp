c14de61ef03f34845f3c55e59d2eb98edb7c6adb
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-21 23:43:02 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (91.66%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -4,11 +4,11 @@ public:
         int count_caps = 0;
 
     for (char ch : word) {
-        if (std::isupper(ch)) {
+        if (isupper(ch)) {
             count_caps++;
         }
     }
 
-    return (count_caps == word.length() || count_caps == 0 || (count_caps == 1 && std::isupper(word[0])));
+    return (count_caps == word.length() || count_caps == 0 || (count_caps == 1 && isupper(word[0])));
     }
 };
