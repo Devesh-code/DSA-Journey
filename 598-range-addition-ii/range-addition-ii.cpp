d36bddce2a517438dfd46eb1e3ce0ef6614f1ea0
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-11-24 18:58:17 +0530

    Time: 8 ms (70.45%) | Memory: 11.2 MB (85.83%) - LeetSync

---
+++ 598-range-addition-ii/range-addition-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+    int row = m;
+    int col = n;
+
+    for (const auto& op : ops) {
+        row = min(row, op[0]);
+        col = min(col, op[1]);
+    }
+
+    return row * col;
+    }
+};
