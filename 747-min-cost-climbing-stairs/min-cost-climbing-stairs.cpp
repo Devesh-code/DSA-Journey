5e536926c41ee274dfa9c797cfdfd6e39402d558
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-12-03 02:28:07 +0530

    Time: 3 ms (84.43%) | Memory: 14.1 MB (28.75%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -10,7 +10,7 @@ public:
 
         // Calculate minimum cost for each step
         for (int i = 2; i < n; ++i) {
-            dp[i] = std::min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
+            dp[i] = min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
         }
 
         // The minimum cost to reach the top is the minimum of the last two steps
