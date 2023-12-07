c6a07f9c1db0b9c8f77905e13f3a0fdcc590112a
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-12-07 19:12:58 +0530

    Time: 3 ms (95.28%) | Memory: 8 MB (68.09%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -21,3 +21,25 @@ private:
         return 0.5 * abs(p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1]));   
     }
 };
+
+/*
+To solve this problem, you can use the Shoelace Formula to calculate the area of a triangle given its three vertices. The Shoelace Formula states that if the vertices of a triangle are (x1, y1), (x2, y2), and (x3, y3), then the area (A) is given by:
+
+A = 1/2∣x1⋅(y2−y3)+x2⋅(y3−y1)+x3⋅(y1−y2)∣
+
+Now, to find the largest triangle area, you can iterate over all possible combinations of three points and calculate the area using the Shoelace Formula. Keep track of the maximum area encountered.
+
+Here's a step-by-step explanation:
+
+Initialize a variable maxArea to store the maximum area encountered, and set it to 0.
+
+Iterate over all combinations of three points using three nested loops.
+
+For each combination of three points, calculate the area using the Shoelace Formula.
+
+Update maxArea if the calculated area is greater than the current maximum.
+
+After iterating over all combinations, maxArea will contain the maximum area of a triangle formed by any three points.
+
+Return maxArea.
+*/
