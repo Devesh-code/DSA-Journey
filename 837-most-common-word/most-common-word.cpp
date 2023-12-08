f76f27542be0b987e5b294daf0b251bcb41a0858
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-12-09 00:14:54 +0530

    Time: 3 ms (80.23%) | Memory: 7.9 MB (87.25%) - LeetSync

--- 837-most-common-word/most-common-word.cpp
+++ 837-most-common-word/most-common-word.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     string mostCommonWord(string paragraph, vector<string>& banned) {
-        for(auto&c : paragraph)
-		c = ispunct(c) ? ' ' : tolower(c);
+           // Preprocess the paragraph
+        for (auto& c : paragraph)
+            c = ispunct(c) ? ' ' : tolower(c);
 
-	string str, res;
-	unordered_map<string, int> freq;
-	unordered_set<string> b(banned.begin(), banned.end());
-	stringstream ss(paragraph);
+        std::string str, res;
+        std::unordered_map<std::string, int> freq;
+        std::unordered_set<std::string> b(banned.begin(), banned.end());
 
-	while(ss>>str)
-		if(b.count(str)==0 && freq[res]<++freq[str]) 
-			res = str;
+        auto it = paragraph.begin();
+        while (it != paragraph.end()) {
+            // Find the beginning of the next word
+            while (it != paragraph.end() && isspace(*it))
+                ++it;
 
-	return res;
+            // Find the end of the current word
+            auto word_end = it;
+            while (word_end != paragraph.end() && !isspace(*word_end))
+                ++word_end;
+
+            // Extract the current word
+            str.assign(it, word_end);
+
+            // Move the iterator to the next word
+            it = word_end;
+
+            // Check if the word is not banned and update frequencies
+            if (b.count(str) == 0 && freq[res] < ++freq[str])
+                res = str;
+        }
+
+        return res;
     }
 };
