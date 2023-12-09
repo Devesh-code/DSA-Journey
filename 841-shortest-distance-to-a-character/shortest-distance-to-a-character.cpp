ba5bf5583b90d22e9faaf49298bffe4792148a38
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2023-12-10 01:08:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (84.48%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
@@ -29,3 +29,24 @@ public:
         return answer; 
     }
 };
+
+/*
+
+To solve this problem, you can iterate through the string from left to right and then from right to left. For each position, find the distance to the closest occurrence of the target character in both directions and choose the minimum of the two distances.
+
+Here's the step-by-step approach:
+
+Initialize an array answer of the same length as the input string s to store the distances.
+
+Iterate from left to right:
+
+Initialize a variable prev to -1, representing the last occurrence of the target character.
+For each character in s, update the distance to the closest occurrence of c using the formula abs(i - prev).
+Update prev if the current character is equal to c.
+Iterate from right to left:
+
+Initialize a variable prev to -1.
+For each character in s (from right to left), update the distance to the closest occurrence of c using the formula abs(i - prev) and choose the minimum with the current value in answer[i].
+Update prev if the current character is equal to c.
+The answer array now contains the distances to the closest occurrences of the target character in both directions.
+*/
