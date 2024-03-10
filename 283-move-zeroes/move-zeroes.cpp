d15ac707fc8a78fa579c8782da1f60817069faff
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-03-11 01:12:16 +0530

    Time: 13 ms (77.39%) | Memory: 21.6 MB (31.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,37 +2,42 @@ class Solution {
 public:
   void moveZeroes(vector<int>& nums) {
     int n = nums.size();
-    // Brute force: 
-    // By using a temp vector to store all the none zero elements 
+    // // Brute force: 
+    // // By using a temp vector to store all the none zero elements 
 
-    // Step1: coping all the nonzero elements to temp
-    vector<int>temp;
-    for(int i = 0; i < n; i++){
-        if(nums[i] != 0){
-            temp.push_back(nums[i]);
-        }
-    }
-    // Step2: cpoing back to the original vector
-    int nz= temp.size();
-    for(int i = 0; i < nz; i++){
-        nums[i] = temp[i];
-    }
-    // Step3: Making all left elements zero
-    for(int i = nz; i < n; i++){
-        nums[i] = 0;
-    }
+    // // Step1: coping all the nonzero elements to temp
+    // vector<int>temp;
+    // for(int i = 0; i < n; i++){
+    //     if(nums[i] != 0){
+    //         temp.push_back(nums[i]);
+    //     }
+    // }
+    // // Step2: cpoing back to the original vector
+    // int nz= temp.size();
+    // for(int i = 0; i < nz; i++){
+    //     nums[i] = temp[i];
+    // }
+    // // Step3: Making all left elements zero
+    // for(int i = nz; i < n; i++){
+    //     nums[i] = 0;
+    // }
+
 
     // Optimal approach: Using two pointers 
+    int j = -1;
+    for(int i = 0; i < n; i++){
+        if(nums[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    if(j==-1) return;
 
-    
-    // int l = 0; 
-    // for (int r = 1; r < n; r++) { // checking elements at every iteration if it is zero them swap 
-    //   if (nums[r] != 0) {
-    //     if (l != r) {
-    //       swap(nums[l], nums[r]);
-    //     }
-    //     ++l;
-    //   }
-    // }
+    for(int i = j + 1; i < n; i++){
+        if(nums[i] != 0){
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
   }
 };
