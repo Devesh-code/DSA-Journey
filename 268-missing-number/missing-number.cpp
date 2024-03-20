2f80b488103229e096ea30fcb11044fd2be96184
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-03-21 01:09:06 +0530

    Time: 11 ms (80.71%) | Memory: 20.4 MB (33.70%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -14,12 +14,59 @@
 
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+
+//     int missingNumber(vector<int>& nums) { // brute force approach and can produce errors
+//     int flag = 0;
+//     int n = nums.size();
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 0; j < n - 1; j++) {
+//             if (nums[j] == i) {
+//                 flag = 1;
+//                 break;
+//             }
+//         }
+//         if (flag == 0) return i;
+//     }
+//     return -1;
+// }
+
+
+    // A bit better approach
+
+    // int missingNumber(vector<int>& nums){
+    //     int n = nums.size();
+    //     int hash[n+1] = {0};
+
+    //     for(int i = 0; i < n-1; i++){
+    //         hash[nums[i]] = 1;
+    //     }
+
+    //     for(int j = 1; i <= n; i++){
+    //         if(hash[i] == 0) return i;
+    //     }
+    //     return -1;
+    // }
+
+
+    // Two diffrent optimal approaches sum & Xor
+
+    int missingNumber(vector<int>& nums){
         int n = nums.size();
-        int result = n;
-        for (int i = 0; i < n; i++) {
-            result ^= i ^ nums[i];
+        int expected_sum = n * (n + 1)/2 ; // 2
+        int actual_sum = 0;
+        for(int i = 0; i < n; i++){
+            actual_sum += nums[i];
         }
-        return result;
+        return expected_sum - actual_sum;
     }
+
+
+    // int missingNumber(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int result = n;
+    //     for (int i = 0; i < n; i++) {
+    //         result ^= i ^ nums[i];
+    //     }
+    //     return result;
+    // }
 };
