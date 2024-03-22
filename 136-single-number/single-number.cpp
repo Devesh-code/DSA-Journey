585479115f07c50112b852b89c3038a1ea27743a
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-03-23 01:03:40 +0530

    Time: 14 ms (61.55%) | Memory: 19.2 MB (60.04%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -18,14 +18,23 @@ public:
 
         // >>>>>>>>>> Better <<<<<<<<<<<<<
 
-        unordered_map<int , int> mp;
-        for (int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
+        // unordered_map<int , int> mp;
+        // for (int i = 0; i < nums.size(); i++){
+        //     mp[nums[i]]++;
+        // }
 
-        for(auto it:mp){
-            if(it.second == 1) return it.first;
+        // for(auto it:mp){
+        //     if(it.second == 1) return it.first;
+        // }
+        // return -1;
+
+
+        // >>>>>>>>>>> Optimal <<<<<<<<<<<<<<
+
+        int xr = 0;
+        for (int i = 0; i < nums.size(); i++){
+            xr ^= nums[i];
         }
-        return -1;
+        return xr;
     }
 };
