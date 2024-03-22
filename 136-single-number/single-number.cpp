b9a6e374c5c4778963e01a1071d15339b0294bd7
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-03-23 01:00:38 +0530

    Time: 26 ms (15.06%) | Memory: 23.6 MB (5.72%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // The Brute Force:
-        for(int i = 0; i < nums.size(); i++){
-          int nm = nums[i];
-          int cnt = 0;
-            for(int j = 0; j < nums.size(); j++){
-                if(nums[j] == nm){
-                    cnt++;
-                }
-            }
-        if(cnt == 1) return nm;
+        
+        // >>>>>>>>> The Brute Force <<<<<<<<<<<<<
+
+        // for(int i = 0; i < nums.size(); i++){
+        //   int nm = nums[i];
+        //   int cnt = 0;
+        //     for(int j = 0; j < nums.size(); j++){
+        //         if(nums[j] == nm){
+        //             cnt++;
+        //         }
+        //     }
+        // if(cnt == 1) return nm;
+        // }
+        // return -1;
+
+        // >>>>>>>>>> Better <<<<<<<<<<<<<
+
+        unordered_map<int , int> mp;
+        for (int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+
+        for(auto it:mp){
+            if(it.second == 1) return it.first;
         }
         return -1;
     }
