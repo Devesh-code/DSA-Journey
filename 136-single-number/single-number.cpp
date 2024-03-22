a93dfaa38ed9abfb4632714a104b5d61e8159805
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-03-23 00:56:37 +0530

    Time: 603 ms (5.01%) | Memory: 19.2 MB (60.04%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-   int singleNumber(vector<int>& nums) {
-  // The Brute Force (modified):
-  for (int i = 0; i < nums.size(); i++) {
-    int nm = nums[i];
-    int cnt = 0;
-    for (int j = 0; j < nums.size(); j++) {
-      if (nums[j] == nm) { // Check for non-self match
-        cnt++;
-      }
+    int singleNumber(vector<int>& nums) {
+        // The Brute Force:
+        for(int i = 0; i < nums.size(); i++){
+          int nm = nums[i];
+          int cnt = 0;
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[j] == nm){
+                    cnt++;
+                }
+            }
+        if(cnt == 1) return nm;
+        }
+        return -1;
     }
-    if (cnt == 1) return nm;  // Return the single number if found
-  }
-  // No single number found (all elements appear twice)
-  return -1;
-}
-
 };
