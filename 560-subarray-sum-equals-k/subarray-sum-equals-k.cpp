f56cbea3f41b6bc66924aa5860291364a06a2a94
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-03-27 02:05:35 +0530

    Time: 68 ms (61.04%) | Memory: 45.1 MB (58.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // The Brute Force 
-        int len = 0;
-        int flag = 0;
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
-            int s = 0;
-            for(int j = i; j < n; j++){
-                s += nums[j];
-                if(s == k){
-                    flag += 1;
-                }
-            }
-        }
-        return flag;
+        // The Brute Force(Simply recreating every possible subarray)
+        // int len = 0;
+        // int flag = 0;
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++){
+        //     int s = 0;
+        //     for(int j = i; j < n; j++){
+        //         s += nums[j];
+        //         if(s == k){
+        //             flag += 1;
+        //         }
+        //     }
+        // }
+        // return flag;
+
+        // The Better (By using Hashing )
+        int i=0,j=0,flag=0,n=size(nums),sum=0;
+        unordered_map<int,int>mp;
+        while(j<n){
+           sum+=nums[j];
+           if(sum==k)flag++;
+           if(mp.find(sum-k)!=mp.end())flag+=mp[sum-k];
+           mp[sum]++;
+           j++;
+       } 
+       return flag;
+
+        // The Optimal way (Using Two pointer,sliding window & greedy approach) not suted for this problem
+
+        // int left = 0, right = 0; 
+        // int sum = nums[0];
+        // int n = nums.size();
+        // int flag = 0;
+        // while(right < n){
+        //     while(left <= right && sum > k){
+        //         sum -= nums[left];
+        //         left++;
+        //     }
+        //     if(sum == k) flag += 1;
+        //     right++;
+        //     if(right < n){
+        //         sum += nums[right];
+        //     }
+
+        // }
+        // return flag;
     }
 };
