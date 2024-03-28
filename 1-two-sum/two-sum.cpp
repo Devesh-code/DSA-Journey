688dfcd811e9edbe2a4eecb514ab702d96de3ddb
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-03-29 03:19:48 +0530

    Time: 11 ms (65.14%) | Memory: 14.1 MB (17.12%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>result = {};
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                int sum = 0;
-                sum = nums[i] + nums[j];
-                if( sum == target && i != j){
-                    result.push_back(i);
-                    result.push_back(j);
-                }
 
+        // The Brute force by using two pointer approach 
+        // vector<int>result = {};
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i; j < n; j++){
+        //         int sum = 0; 
+        //         sum = nums[i] + nums[j];
+        //         if( sum == target && i != j){
+        //             result.push_back(i);
+        //             result.push_back(j);
+        //         }
+
+        //     }
+        // }
+        // return result;
+
+
+        // The optimal way by using hashing with the help of hashmap
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int moreNeeded = target - num;
+            if (mpp.find(moreNeeded) != mpp.end()) {
+                return {mpp[moreNeeded], i};
             }
+            mpp[num] = i;
         }
-        return result;
+        return { -1, -1};
     }
 };
