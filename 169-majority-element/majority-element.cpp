b50da83db62a3fa753c94a5b1e83e7e89742a87c
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-04-01 02:33:24 +0530

    Time: 17 ms (37.75%) | Memory: 26.7 MB (25.96%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-         //size of the given array:
+        // The simple Brute force by counting occurences of element;
         int n = nums.size();
-        int cnt = 0; // count
-        int el; // Element
+        // int cnt = 0;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n; j++){
+        //         if(nums[i] == nums[j]) cnt++;
 
-        //applying the algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = nums[i];
-            }
-            else if (el == nums[i]) cnt++;
-            else cnt--;
+        //         if(cnt > (n/2)) return nums[i];
+        //     }
+        // }
+        // return -1;
+
+        // The Better way by using hashing;
+        map<int, int>mpp;
+        for(int i = 0; i < n; i++)
+        {
+            mpp[nums[i]]++;
+        }        
+        for(auto it: mpp){
+            if(it.second > n/2) return it.first;
         }
-        return el;
+        return -1;
+
+        //Optimal solution: 
+        // int n = nums.size();
+        // int cnt = 0; 
+        // int el; 
+
+        // //applying the algorithm: Moose voting algo
+        // for (int i = 0; i < n; i++) {
+        //     if (cnt == 0) {
+        //         cnt = 1;
+        //         el = nums[i];
+        //     }
+        //     else if (el == nums[i]) cnt++;
+        //     else cnt--;
+        // }
+        // return el;
     }
 };
