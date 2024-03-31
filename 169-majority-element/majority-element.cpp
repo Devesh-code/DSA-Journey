b8a4dd336cf9736ffe12c07dc6ab1a9da7e1c93b
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-04-01 01:21:09 +0530

    Time: 12 ms (73.92%) | Memory: 26.7 MB (25.96%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       int res = 0; // Initialize the result to 0.
-        int count = 0; // Initialize the count to 0.
+         //size of the given array:
+        int n = nums.size();
+        int cnt = 0; // count
+        int el; // Element
 
-        // Iterate through the elements in the 'nums' vector.
-        for (int n : nums) {
-            if (count == 0) {
-                // If the count reaches 0, update the result to the current element 'n'.
-                res = n;
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
             }
-            // Increment or decrement the count based on whether the current element matches the result.
-            count += (n == res ? 1 : -1);
+            else if (el == nums[i]) cnt++;
+            else cnt--;
         }
-
-        // Return the majority element.
-        return res;
+        return el;
     }
 };
