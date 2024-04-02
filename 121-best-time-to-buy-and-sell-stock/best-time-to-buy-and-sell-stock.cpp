2fed83bed8868718f0eee38c23709d86053aa700
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-04-03 01:07:38 +0530

    Time: 78 ms (86.94%) | Memory: 95.7 MB (69.29%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       int maxP = 0; // Initialize the maximum profit as 0.
 
-    // Initialize two pointers, 'l' and 'r', both initially at index 0.
-    int l = 0;
-    int r = 0;
+        // The brute force with O(N2) TC and So time limit exceeded
+        // int maxi = 0;
+        // int n = prices.size();
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++){
+        //         if(prices[j] > prices[i]) 
+        //         maxi = max(prices[j] - prices[i], maxi);
+        //     }
+        // }
+        // return maxi;
 
-    // Loop while the 'r' pointer is less than the length of the 'prices' vector.
-    while (r < prices.size()) {
-        // Check if the price at 'l' is less than the price at 'r', indicating a potential profit.
-        if (prices[l] < prices[r]) {
-            // Calculate the profit by selling at the price 'r' and buying at the price 'l'.
-            int profit = prices[r] - prices[l];
+        // The optimal way:
+        /* We will linearly travel the array. 
+        We can maintain a minimum from the start of the array and compare it
+        with every element of the array, if it is greater than the minimum then
+        take the difference and maintain it in max, otherwise update the minimum. */
+        int maxi = 0;
+        int n = prices.size();
+        int minPrice = INT_MAX;
 
-            // Update 'maxP' to be the maximum of its current value and the calculated profit.
-            maxP = std::max(maxP, profit);
-        } else {
-            // If the price at 'l' is not less than the price at 'r', move the 'l' pointer to the 'r' position.
-            l = r;
+        for (int i = 0; i < prices.size(); i++) {
+            minPrice = min(minPrice, prices[i]);
+            maxi = max(maxi, prices[i] - minPrice);
         }
-
-        // Move the 'r' pointer to the next position.
-        r++;
-    }
-
-    // Return the maximum profit found.
-    return maxP;  
+    
+        return maxi; 
     }
 };
