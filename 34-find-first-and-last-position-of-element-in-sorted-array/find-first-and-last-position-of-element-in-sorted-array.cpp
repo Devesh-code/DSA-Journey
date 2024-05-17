486dca44dece81d3ec58b6bd5d43944a607ee0ea
Devesh Sharma <59760503+Devesh-code@users.noreply.github.com>
2024-05-18 00:42:07 +0530

    Time: 7 ms (48.13%) | Memory: 16.1 MB (48.04%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -21,57 +21,57 @@ public:
 
 
      // Optimal1 : using lower(which is nums[index]>=target) & upper(which is nums[index]>target) bound stl functions
-        // int n = nums.size();
-        // int first = -1;
-        // int last = -1;
-        // first = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
-        // last = upper_bound(nums.begin(),nums.end(), target) - nums.begin();
-        // if (first == n || nums[first] != target) return {-1,-1};
-        // return{first,last - 1};
+        int n = nums.size();
+        int first = -1;
+        int last = -1;
+        first = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        last = upper_bound(nums.begin(),nums.end(), target) - nums.begin();
+        if (first == n || nums[first] != target) return {-1,-1};
+        return{first,last - 1};
 
 
 
     //Optimal2:  The main binary search way 
 
-        int first = firstOccur(nums,nums.size(),target);
-        if (first == -1) return {-1, -1};
-        int last = lastOccur(nums,nums.size(),target);
-        return {first,last};
+        // int first = firstOccur(nums,nums.size(),target);
+        // if (first == -1) return {-1, -1};
+        // int last = lastOccur(nums,nums.size(),target);
+        // return {first,last};
 
     }
 
-    int firstOccur(vector<int>& nums,int n, int target)
-    {
-        int low = 0;
-        int high = n -1;
-        int first = -1;
-        while (low <= high){
-            int mid = (low + high)/2;
-            if (nums[mid] == target){
-                first = mid;
-                high = mid - 1;
-            }
-            else if (nums[mid] < target) low = mid + 1;
-            else high = mid - 1;
-        }
-        return first;
-    }
+    // int firstOccur(vector<int>& nums,int n, int target)
+    // {
+    //     int low = 0;
+    //     int high = n -1;
+    //     int first = -1;
+    //     while (low <= high){
+    //         int mid = (low + high)/2;
+    //         if (nums[mid] == target){
+    //             first = mid;
+    //             high = mid - 1;
+    //         }
+    //         else if (nums[mid] < target) low = mid + 1;
+    //         else high = mid - 1;
+    //     }
+    //     return first;
+    // }
 
-    int lastOccur(vector<int> &nums, int n, int target)
-    {
-        int low = 0, high = n - 1;
-        int last = -1;
-        while(low <= high)
-        {
-            int mid = (low + high)/2;
-            if(nums[mid] == target)
-            {
-                last = mid;
-                low = mid + 1;
-            }
-            else if (nums[mid] < target) low = mid + 1;
-            else high = mid - 1;
-        }
-        return last;
-    }
+    // int lastOccur(vector<int> &nums, int n, int target)
+    // {
+    //     int low = 0, high = n - 1;
+    //     int last = -1;
+    //     while(low <= high)
+    //     {
+    //         int mid = (low + high)/2;
+    //         if(nums[mid] == target)
+    //         {
+    //             last = mid;
+    //             low = mid + 1;
+    //         }
+    //         else if (nums[mid] < target) low = mid + 1;
+    //         else high = mid - 1;
+    //     }
+    //     return last;
+    // }
 };
